6ceca513c2cbeaab7460a34cf18446f0052d957d
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-06-11 02:02:16 +0400

    layout (header - footer)

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faLinkedin, faPinterest, faSquareFacebook, faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  protected readonly faSquareFacebook = faSquareFacebook;
+  protected readonly faTwitter = faTwitter;
+  protected readonly faLinkedin = faLinkedin;
+  protected readonly faPinterest = faPinterest;
 }
