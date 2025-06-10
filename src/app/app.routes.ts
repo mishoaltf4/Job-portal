4569130e0d87ff07e3219d5c4ebf3fdd69c5f07a
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-06-11 00:40:53 +0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {ErrorComponent} from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent },
+  {path: '**', component: ErrorComponent}
+];
